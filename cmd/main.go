bec710c7d8a786f51e607476e7c2bae9eadb35ff
gr601_govvi <mrvitshell@gmail.com>
2021-04-09 17:35:03 +0700

    add proc file

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,12 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 
+	err := http.ListenAndServe(":8090", nil)
+	if err == nil {
+		fmt.Println("Error creating http server:", err)
+	}
+	fmt.Println("200")
+
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(os.Stdout)
 
